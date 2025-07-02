984bacec30a1798d4287b61f516ac5df0e31276f
Pavan Maguluri <pavanmaguluri10021999@gmail.com>
2025-07-02 23:07:50 +0530

    Added User model and user API routes

---
+++ OneDrive/Documents/uvp-backend/models/User.js
@@ -0,0 +1,21 @@
+// models/User.js
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  whatsappNumber: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
